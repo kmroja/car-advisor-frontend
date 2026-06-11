2680e40ba435c4413730d3f7b8517048be360313
kmroja <rojakmr25@gmail.com>
2026-06-11 23:21:34 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ import recommendationRoutes from './routes/recommendationRoutes.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+const PORT = process.env.PORT || 4000;
 app.use('/api', recommendationRoutes);
-app.listen(5000,()=>{
-    console.log("Server is running on port 5000");
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
 })
